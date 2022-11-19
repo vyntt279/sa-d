d33f32926ba4631a888148627d30fac50d44b658
vyntt <vyntt@inspirelab.io>
2022-11-19 13:41:09 +0700

    feat(frontend): Init tailwindcss and antd

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import { Layout } from 'antd';
+
+const { Header, Footer, Content } = Layout;
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  )
+}
+
+export default HomePage;
