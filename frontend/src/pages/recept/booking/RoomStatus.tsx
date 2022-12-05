f15416e5f5f1260366f87dae49d7d61454af54dd
vyntt279 <vy.ngonttv2709@hcmut.edu.vn>
2022-12-05 23:53:39 +0700

    feat(customer): update booking of recept

--- frontend/src/pages/recept/booking/RoomStatus.tsx
+++ frontend/src/pages/recept/booking/RoomStatus.tsx
@@ -1,5 +1,61 @@
-const RoomStatus = () => {
+import { useEffect, useState } from "react"
+import { Button, Popover, Tag } from "antd"
+
+import { RoomCard } from 'components/roomList/RoomList'
+import { url } from 'stores/constant'
+import { render } from "@testing-library/react"
+
+type RoomStatusProps = {
+  roomNum: string
+}
+
+const RoomStatus = ({ roomNum }: RoomStatusProps) => {
+  const [data, setData] = useState<RoomCard>();
+  const getRoomInfo = async () => {
+    await fetch(url + `/rooms/${roomNum}`, {
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "GET"
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error == undefined) {
+          setData(res)
+        }
+      });
+  }
+  useEffect(() => {
+    getRoomInfo()
+  })
+
+  const renderStatus = (status: string) => {
+    var color = status == 'available' ? 'green' : 'geekblue';
+    if (status === 'loser') {
+      color = 'volcano';
+    }
+    return (
+      <Tag color={color} key={status}>
+        {status != undefined ? status.toUpperCase() : "UNDEFINED"}
+      </Tag>
+    );
+  }
+
   return (<>
+    <Popover content={data != undefined? (<div>
+      Room type: {data?.type}
+      <br />
+      Price per day: {data?.price}
+      <br />
+      Room Number: {data?.roomNum}
+      <br />
+      Status: {renderStatus(data?.status)}
+      <br />
+    </div>) : "Cannot get information of this room"} title={`Room ${roomNum}`}>
+      <Button >{roomNum}</Button>
+    </Popover>
   </>)
 }
 
--- frontend/src/pages/recept/booking/ViewBookings.tsx
+++ frontend/src/pages/recept/booking/ViewBookings.tsx
@@ -1,9 +1,23 @@
-import { Space, Table, Tag, notification, Button } from 'antd'
+import { Space, Table, Tag, notification, Button, Statistic } from 'antd'
 import type { ColumnsType } from 'antd/es/table';
 
 import { url } from 'stores/constant'
 import { useEffect, useState } from 'react';
 import { BookingListItem } from 'pages/customer/bookList/BookList';
+import RoomStatus from './RoomStatus';
+
+const renderPayment = (status: string) => {
+  var color = status == 'paid' ? 'green' : 'geekblue';
+  if (status === 'unpaid') {
+    color = 'volcano';
+  }
+  return (
+    <Tag color={color} key={status}>
+      {status != undefined ? status.toUpperCase() : "UNDEFINED"}
+    </Tag>
+  );
+}
+
 const columns: ColumnsType<BookingListItem> = [
   {
     title: 'ID',
@@ -14,23 +28,39 @@ const columns: ColumnsType<BookingListItem> = [
     title: 'Room Number',
     dataIndex: 'roomNum',
     key: 'roomNum',
+    render: (_, { roomNum }) => {
+      return (
+        <RoomStatus roomNum={roomNum} />
+      )
+    }
+  },
+  {
+    title: 'Cost',
+    dataIndex: 'roomNum',
+    key: 'roomNum',
+    render: (_, { roomNum }) => {
+      return (<>
+        <Statistic value={roomNum} prefix='$' valueStyle={{ fontSize: '1em' }} />
+        {renderPayment('paid')}
+      </>
+
+      )
+    }
   },
   {
     title: 'Status',
     key: 'status',
     dataIndex: 'status',
     render: (_, { status }) => {
-      {
-        var color = status = 'awaiting' ? 'green' : 'geekblue';
-        if (status === 'loser') {
-          color = 'volcano';
-        }
-        return (
-          <Tag color={color} key={status}>
-            {status != undefined? status.toUpperCase(): "UNDEFINED"}
-          </Tag>
-        );
+      var color = status == 'awaiting' ? 'green' : 'geekblue';
+      if (status === 'loser') {
+        color = 'volcano';
       }
+      return (
+        <Tag color={color} key={status}>
+          {status != undefined ? status.toUpperCase() : "UNDEFINED"}
+        </Tag>
+      );
     }
   },
   {
@@ -44,11 +74,51 @@ const columns: ColumnsType<BookingListItem> = [
     ),
   },
 ]
-const handleCheckIn = () => {
+const handleCheckIn = async () => {
+  // await fetch(url + "/bookings/getAllBooking", {
+  //   mode: "cors",
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json',
+  //     'Authorization': 'Bearer ' + localStorage.getItem('authorization')
+  //   },
+  //   method: "GET",
+  // })
+  //   .then((response) => response.json())
+  //   .then((response) => {
+  //     console.log('Data', response)
 
+  //   })
+  //   .catch((reason) => {
+  //     console.log(reason)
+  //     notification.info({
+  //       message: `Cannot check in, please try again`,
+  //       placement: 'top',
+  //     });
+  //   })
 }
-const handleCheckOut = () => {
+const handleCheckOut = async () => {
+  // await fetch(url + "/bookings/getAllBooking", {
+  //   mode: "cors",
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json',
+  //     'Authorization': 'Bearer ' + localStorage.getItem('authorization')
+  //   },
+  //   method: "GET",
+  // })
+  //   .then((response) => response.json())
+  //   .then((response) => {
+  //     console.log('Data', response)
 
+  //   })
+  //   .catch((reason) => {
+  //     console.log(reason)
+  //     notification.info({
+  //       message: `Cannot check out, please try again`,
+  //       placement: 'top',
+  //     });
+  //   })
 }
 
 const ViewBooking = () => {
