e7bb8cca223175dc8aad5fca1e23325574b32ff8
EmChes <emches1976@gmail.com>
2022-12-06 00:03:03 +0700

    2022-12-06 v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import Home from 'pages/customer/home/Home'
 import BookList from "pages/customer/bookList/BookList";
 import RoomList from "pages/customer/roomList/RoomList";
 import MyLayout from "layouts/MyLayout";
+import Admin from "pages/admin/Admin"
 
 const ROLES = {
   'UNSPECIFIED': 0,
@@ -19,23 +20,24 @@ const ROLES = {
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="login" element={<LogIn />} />
-      <Route path="signup" element={<SignUp />} />
-      <Route path="/" element={<MyLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route element={<RequireAuth allowedRoles={[ROLES.CUSTOMER]} />}>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route path="bookings" element={<BookList />} />
-          <Route path="rooms" element={<RoomList />} />
-        </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.ADMIN]} />}>
+    // <Routes>
+    //   <Route path="login" element={<LogIn />} />
+    //   <Route path="signup" element={<SignUp />} />
+    //   <Route path="/" element={<MyLayout />}>
+    //     <Route path="/" element={<Home />} />
+    //     <Route element={<RequireAuth allowedRoles={[ROLES.CUSTOMER]} />}>
+    //       {/* <Route path="/" element={<Home />} /> */}
+    //       <Route path="bookings" element={<BookList />} />
+    //       <Route path="rooms" element={<RoomList />} />
+    //     </Route>
+    //     <Route element={<RequireAuth allowedRoles={[ROLES.ADMIN]} />}>
 
-        </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.RECEIPTIONIST]} />}>
-        </Route>
-      </Route>
-    </Routes>
+    //     </Route>
+    //     <Route element={<RequireAuth allowedRoles={[ROLES.RECEIPTIONIST]} />}>
+    //     </Route>
+    //   </Route>
+    // </Routes>
+    <Admin></Admin>
   )
 }
 
